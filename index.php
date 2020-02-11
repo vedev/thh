23ca7fef503c4f9b7f768aeecd1faf3cbe987c08
Roman Krylov <xtsidx@gmail.com>
2020-02-11 10:34:01 +0300

    Asterix support
    use asterix for subdomain

--- index.php
+++ index.php
@@ -16,6 +16,14 @@ $host = ltrim(mb_strtolower($host), 'www.');
 $scheme = isSecure() ? 'https' : 'http';
 $target = $thh->target($host);
 
+// find with subdomain by asterix
+if(!is_string($target)) {
+    preg_match('/^([a-zA-Z0-9-_]+)\.([a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+)$/', $host, $matches);
+    if(count($matches) == 3) {
+        $target = $matches[1] . '.' . $thh->target('*.'.$matches[2]);
+    }
+}
+
 if (is_string($target))
     header("Location: $scheme://$target".$_SERVER['REQUEST_URI'], true, 301);
 else
